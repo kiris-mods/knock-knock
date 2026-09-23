b66cf7f1ebadbb1662296e1d55b9c6927a12d961
KiriCattus <19393068+KiriCattus@users.noreply.github.com>
2026-09-24 00:39:21 +0100

    Fix NeoForge meta file and swap license links.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ val tag = providers.environmentVariable("TAG")
 val buildNumber = providers.environmentVariable("BUILD_NUMBER").map { "build.${it}" }
 
 version = tag.orElse(provider { buildString {
-    append("4.0.0-development")
+    append("4.0.1-development")
     if(!tag.isPresent) {
         append(buildNumber.map { "+${it}" }.getOrElse(""))
     }
